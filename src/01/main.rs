f8288e7b952ff66233e64a09863896e9bb85cef6
Michael Vrána <vranami8@fit.cvut.cz>
2024-12-25 17:27:58 +0100

    common runner

--- src/01/main.rs
+++ src/01/main.rs
@@ -1,7 +1,10 @@
+use aoc_2024::{InputParser, ProblemSolver, Runner};
 use regex::Regex;
-use std::{collections::HashMap, fs::read_to_string, iter::zip};
+use std::{collections::HashMap, fmt::Display, fs::read_to_string, iter::zip};
 
 type Input = (Vec<u32>, Vec<u32>);
+
+struct Parser;
 
 fn read_lines(filename: &str) -> Vec<String> {
     match read_to_string(filename) {
@@ -10,20 +13,22 @@ fn read_lines(filename: &str) -> Vec<String> {
     }
 }
 
-fn parse_input(file: &str) -> Input {
-    let regex = Regex::new(r"^([0-9]+)\s+([0-9]+)$").unwrap();
+impl InputParser<Input> for Parser {
+    fn parse(&mut self, filename: &str) -> Input {
+        let regex = Regex::new(r"^([0-9]+)\s+([0-9]+)$").unwrap();
 
-    let mut left_vec = Vec::<u32>::new();
-    let mut right_vec = Vec::<u32>::new();
+        let mut left_vec = Vec::<u32>::new();
+        let mut right_vec = Vec::<u32>::new();
 
-    for line in read_lines(file) {
-        let captures = regex.captures(line.as_str()).unwrap();
+        for line in read_lines(filename) {
+            let captures = regex.captures(line.as_str()).unwrap();
 
-        left_vec.push(captures.get(1).unwrap().as_str().parse::<u32>().unwrap());
-        right_vec.push(captures.get(2).unwrap().as_str().parse::<u32>().unwrap());
+            left_vec.push(captures.get(1).unwrap().as_str().parse::<u32>().unwrap());
+            right_vec.push(captures.get(2).unwrap().as_str().parse::<u32>().unwrap());
+        }
+
+        (left_vec, right_vec)
     }
-
-    (left_vec, right_vec)
 }
 
 struct Result {
@@ -31,48 +36,57 @@ struct Result {
     similarity: u32,
 }
 
-fn solve(mut input: Input) -> Result {
-    input.0.sort();
-    input.1.sort();
+impl Display for Result {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "difference is {}, similarity is {}",
+            self.difference, self.similarity
+        )
+    }
+}
 
-    let difference: u32 = zip(input.0.iter(), input.1.iter())
-        .map(|(left, right)| left.abs_diff(*right))
-        .sum();
+struct Solver;
 
-    let counts = input
-        .1
-        .iter()
-        .fold(HashMap::<u32, u32>::new(), |mut counts, value| {
-            match counts.get(value) {
-                Some(count) => counts.insert(*value, count + 1),
-                None => counts.insert(*value, 1),
-            };
+impl ProblemSolver<Input, Result> for Solver {
+    fn solve(&mut self, mut input: Input) -> Result {
+        input.0.sort();
+        input.1.sort();
 
-            counts
-        });
+        let difference: u32 = zip(input.0.iter(), input.1.iter())
+            .map(|(left, right)| left.abs_diff(*right))
+            .sum();
 
-    let similarity = input
-        .0
-        .iter()
-        .map(|value| value * counts.get(value).unwrap_or(&0))
-        .sum();
+        let counts = input
+            .1
+            .iter()
+            .fold(HashMap::<u32, u32>::new(), |mut counts, value| {
+                match counts.get(value) {
+                    Some(count) => counts.insert(*value, count + 1),
+                    None => counts.insert(*value, 1),
+                };
 
-    Result {
-        difference,
-        similarity,
+                counts
+            });
+
+        let similarity = input
+            .0
+            .iter()
+            .map(|value| value * counts.get(value).unwrap_or(&0))
+            .sum();
+
+        Result {
+            difference,
+            similarity,
+        }
     }
 }
 
 fn main() {
-    let input_files = vec!["src/01/input_1.txt", "src/01/input_2.txt"];
-
-    for file in input_files {
-        let input = parse_input(file);
-        let result = solve(input);
-
-        println!(
-            "File {}, difference is {}, similarity is {}",
-            file, result.difference, result.similarity
-        )
-    }
+    Runner::new(
+        &mut Parser,
+        &mut Solver,
+        vec!["src/01/input_1.txt", "src/01/input_2.txt"],
+    )
+    .run();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,40 @@
+use std::fmt::Display;
+
 pub trait InputParser<TProblem> {
-    fn parse(filename: &str) -> TProblem;
+    fn parse(&mut self, filename: &str) -> TProblem;
 }
 
-pub trait Solver<TProblem, TResult: ToString> {
-    fn solve(input: TProblem) -> TResult;
+pub trait ProblemSolver<TProblem, TResult: Display> {
+    fn solve(&mut self, input: TProblem) -> TResult;
 }
 
-pub struct ProblemRunner<'a, TProblem> {
-    filenames: Vec<String>,
-    input_parser: InputParser<TProblem>,
+pub struct Runner<'a, 'b, TProblem, TResult: Display> {
+    input_files: Vec<&'a str>,
+
+    // TODO find a way to remove the dynamic dispatch
+    parser: &'a mut dyn InputParser<TProblem>,
+    solver: &'b mut dyn ProblemSolver<TProblem, TResult>,
+}
+
+impl<'a, 'b, TProblem, TResult: Display> Runner<'a, 'b, TProblem, TResult> {
+    pub fn new(
+        parser: &'a mut dyn InputParser<TProblem>,
+        solver: &'b mut dyn ProblemSolver<TProblem, TResult>,
+        input_files: Vec<&'a str>,
+    ) -> Self {
+        Runner {
+            input_files,
+            parser,
+            solver,
+        }
+    }
+
+    pub fn run(&mut self) {
+        for filename in self.input_files.iter() {
+            let input = self.parser.parse(filename);
+            let result = self.solver.solve(input);
+
+            println!("File {}: {}", filename, result)
+        }
+    }
 }
